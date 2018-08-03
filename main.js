829be76c12e29df425424f6b64024cb9757a7ed2
ganeshpatro321 <ganeshpatro321@gmail.com>
2018-08-03 11:09:00 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,92 @@
+//document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+var el = document.getElementById('issueInputForm');
+if(el){
+  el.addEventListener('submit',saveIssue)
+}
+
+function saveIssue(e) {
+  var issueId = chance.guid();
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueStatus = 'Open';
+
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+
+  if (localStorage.getItem('issues') === null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+
+  document.getElementById('issueInputForm').reset();
+
+  fetchissues();
+
+  e.preventDefault();
+}
+
+function fetchissues() {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesList = document.getElementById('issuesList');
+  console.log(issuesList);
+
+  //document.getElementById('issuesList').innerHTML = '';
+
+  for(var i=0; i<issues.length; i++){
+    var id = issues[i].id;
+    var desc = issues[i].desc;
+    var severity = issues[i].severity;
+    var status = issues[i].status;
+    var assignedTo = issues[i].assignedTo;
+
+    document.getElementById('issuesList').innerHTML += '<div class = "well">'+
+                              '<h6> Issue ID:' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
+                              '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
+                              '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+                              '</div>';
+  }
+}
+
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'))
+
+  for(i=0; i<issues.length; i++){
+    if(issues[i].id == id){
+      issues[i].status = "closed";
+    }
+  }
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchissues();
+}
+
+function deleteIssue (id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for(var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchissues();
+}
